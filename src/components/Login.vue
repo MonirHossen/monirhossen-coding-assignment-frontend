b4fa93a8b9e96689a24a20453d59fdd51e5f8660
Monir Hossen <38731309+MonirHossen@users.noreply.github.com>
2024-03-09 09:58:31 +0600

    add some style on forntend

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container">
+    <div class="form-container">
         <form @submit.prevent="login" class="login-form">
             <input type="email" name="email" placeholder="Email.." v-model="email" />
             <input type="password" name="password" placeholder="Password.." v-model="password" />
@@ -40,3 +40,29 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+/* styling for the outer form container */
+.form-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+/* styling for the login form layout and apperance */
+.login-form {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    width: 300px;
+    padding: 20px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    font-size: 16px;
+}
+/* Specific styling for login button */
+.login-form button {
+    background-color: #007BFF;
+    color: white;
+    cursor: pointer;
+}
+</style>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -3,7 +3,7 @@
         <form @submit.prevent="register" class="register-form">
             <input type="text" name="name" id="name" v-model="name" placeholder="Your name">
             <input type="email" name="email" id="email" v-model="email" placeholder="Your email">
-            <input type="password" name="password" id="password" v-model="password" placeholder="Your password">
+            <input type="password" name="password" id="password" v-model="password" placeholder="Password">
             <button type="submit">Register</button>
         </form>
     </div>
@@ -40,3 +40,29 @@
         }
     }
 </script>
+
+<style scoped>
+/* styling for the outer form container */
+.form-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+/* styling for the login form layout and apperance */
+.register-form {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    width: 300px;
+    padding: 20px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    font-size: 16px;
+}
+/* Specific styling for login button */
+.register-form button {
+    background-color: #007BFF;
+    color: white;
+    cursor: pointer;
+}
+</style>
